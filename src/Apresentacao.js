4d55ea572e9d15cf0ff257ef5db66a1055c06db3
rluciano1965 <86748595+rluciano1965@users.noreply.github.com>
2021-09-10 22:27:26 -0300

    Componente Styled 4 commit

--- src/Apresentacao.js
+++ src/Apresentacao.js
@@ -1,30 +1,42 @@
-import React from 'react';
-import { View, Text, Image } from 'react-native';
-
-
-const Apresentacao = () => {
-
-    const Title = styled.h1`
-  font-size: 1.5em;
-  text-align: center;
-  color: palevioletred;
-`;
-
-   
-    const Wrapper = styled.section`
-  padding: 4em;
-  background: papayawhip;
-`;
-
-   
-    render(
-        <Wrapper>
-            <Title>
-                Cinema em Casa
-            </Title>
-        </Wrapper>
-    );
-
+const MainContainer = styled(View, {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+}, {forwardRef: true});
+ 
+const DefaultText = styled(Text, {
+    fontSize: 15,
+    fontFamily: 'Roboto',
+    color: '#333',
+});
+ 
+const RedText = styled(DefaultText, {
+    color: 'red',
+});
+ 
+const GreenText = styled(DefaultText, {
+    color: 'green',
+});
+ 
+const ColorText = styled(DefaultText, props => ({
+    color: props.color,
+}));
+ 
+class App extends Component {
+ 
+    mainContainerRef;
+ 
+    render() {
+        return (
+            <MainContainer ref={ref => this.mainContainerRef = ref}>
+                <DefaultText>Default Text</DefaultText>
+                <RedText>Red Text</RedText>
+                <GreenText>Green Text</GreenText>
+                <DefaultText style={{fontSize: 20, color: 'green'}}>Big Green Text</DefaultText>
+                <ColorText color='blue'>Color Text</ColorText>
+            </MainContainer>
+        );
+    }
 }
-
-export default Apresentacao;
+ 
+export default App;
